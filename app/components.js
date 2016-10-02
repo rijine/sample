c3316512957682af1ab1a38d88f40d0487f02b7a
RIJIN <e.rijin@gmail.com>
2016-10-02 23:26:12 +0530

    tranclude with player-comp

--- app/components.js
+++ app/components.js
@@ -5,7 +5,8 @@
 		'teamComponent', 
 		{
 			controller:TeamController,
-			template : '<div>{{vm.name}}{{vm.test}}</div>',
+			transclude: true,
+			template : '<div>{{vm.name}}{{vm.test}}</div><div ng-transclude></div>',
 			controllerAs: 'vm',
 			bindings: {
 				name: '='
@@ -15,6 +16,30 @@
 	function TeamController() {
 		var vm=this;
 		vm.test = " Test";
+	}
+
+	angular.module('app.football').component(
+		'playerComponent', 
+		{
+			controller:PlayerController,
+			transclude: true,
+			// require the parent component, in this case accordion component
+			require: {
+				'parent': '^teamComponent'
+			},
+			template : '<div>{{vm.name}}{{vm.test}}</div><div ng-transclude></div>',
+			controllerAs: 'vm',
+			bindings: {
+				name: '='
+			}
+		});
+	PlayerController.$inject = [];
+	function PlayerController() {
+		var vm=this;
+		vm.test = " Test";
+		vm.$onInit = function(){
+			console.log('Player');
+		}
 	}
 
 
