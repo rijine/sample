4c54dba36b7ec31ac7a47e9f50357c75c21cd162
RIJIN <e.rijin@gmail.com>
2016-10-03 00:20:16 +0530

    players to player

--- app/components.js
+++ app/components.js
@@ -17,6 +17,32 @@
 		var vm=this;
 		vm.test = " Test";
 	}
+	
+	angular.module('app.football').component(
+		'playersComponent', 
+		{
+			controller:PlayersController,
+			transclude: true,
+			// require the parent component, in this case accordion component
+			require: {
+				'parent': '^teamComponent'
+			},
+			template : `<div ng-repeat="player in vm.players">
+							<div ng-transclude></div>
+						</div>`,
+			controllerAs: 'vm',
+			bindings: {
+				players: '='
+			}
+		});
+	PlayersController.$inject = [];
+	function PlayersController() {
+		var vm=this;
+		vm.test = " Test";
+		vm.$onInit = function(){
+			console.log('Players');
+		}
+	}
 
 	angular.module('app.football').component(
 		'playerComponent', 
@@ -25,7 +51,7 @@
 			transclude: true,
 			// require the parent component, in this case accordion component
 			require: {
-				'parent': '^teamComponent'
+				'parent': '^playersComponent'
 			},
 			template : '<div>{{vm.name}}{{vm.test}}</div><div ng-transclude></div>',
 			controllerAs: 'vm',
