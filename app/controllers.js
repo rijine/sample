bcfcd1d3142e21da15cac9566765a0562fc6b3d1
RIJIN <e.rijin@gmail.com>
2016-10-02 23:47:06 +0530

    teams list to object

--- app/controllers.js
+++ app/controllers.js
@@ -5,8 +5,61 @@
     angular.module('app.football').controller('HomeCtrl', HomeCtrl);
     HomeCtrl.$inject = [];
     function HomeCtrl() {
-    	var vm=this;
-    	vm.teams = ['1','2','3','4','5'];
+        var vm = this;
+        //vm.teams = ['1','2','3','4','5'];
+        vm.teams = [
+            {
+                name: "X1",
+                players: [
+                    {
+                        name:"name1",
+                        contact:'323234'
+                    },
+                    {
+                        name:"name2",
+                        contact:'323234'
+                    },
+                    {
+                        name:"name3",
+                        contact:'323234'
+                    }
+                ]
+            },
+            {
+                name: "X2",
+                players: [
+                    {
+                        name:"name1",
+                        contact:'323234'
+                    },
+                    {
+                        name:"name2",
+                        contact:'323234'
+                    },
+                    {
+                        name:"name3",
+                        contact:'323234'
+                    }
+                ]
+            },
+            {
+                name: "X3",
+                players: [
+                    {
+                        name:"name1",
+                        contact:'323234'
+                    },
+                    {
+                        name:"name2",
+                        contact:'323234'
+                    },
+                    {
+                        name:"name3",
+                        contact:'323234'
+                    }
+                ]
+            }
+        ];
 
     }
 
